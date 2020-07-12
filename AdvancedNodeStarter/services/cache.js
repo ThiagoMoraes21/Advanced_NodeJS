076f394dcd2c28c261eed7e7c37e4f735c12ecde
thiagoMoraes21 <thiago4x70@gmail.com>
2020-07-12 16:03:07 -0300

    make the cache option toggleable in queries

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -7,8 +7,15 @@ const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 
+mongoose.Query.prototype.cache = function() {
+    this.useCache = true;
+    return this;
+}
+
 mongoose.Query.prototype.exec = async function () {
-    console.log('Running query...');
+    if(!this.useCache) {
+        return exec.apply(this, arguments);
+    }
 
     // this refers to the current query that we a trying to run.
     // ex: Blogs.find({user: id}): this === {user: '123456'}
