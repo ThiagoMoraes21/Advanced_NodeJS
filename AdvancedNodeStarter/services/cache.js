ad7d4f047a455d2bb0b29eb89fa351498c1a7a4c
thiagoMoraes21 <thiago4x70@gmail.com>
2020-07-12 15:50:16 -0300

    add cache system

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,7 +1,37 @@
 const mongoose = require('mongoose');
-const exec = mongoose.Query.prototype.exec;
+const redis = require('redis');
+const util = require('util');
 
-mongoose.Query.prototype.exec = function () {
+const exec = mongoose.Query.prototype.exec;
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+mongoose.Query.prototype.exec = async function () {
     console.log('Running query...');
-    return exec.apply(this, arguments);
+
+    // this refers to the current query that we a trying to run.
+    // ex: Blogs.find({user: id}): this === {user: '123456'}
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), { 
+        collection: this.mongooseCollection.name 
+    }));
+
+    // See if we have a value for 'key' in redis
+    const cacheValue = await client.get(key);
+
+    // if we do, return that
+    if(cacheValue) {
+        const doc = JSON.parse(cacheValue);
+
+        return Array.isArray(doc) ? 
+            doc.map(el => new this.model(el)) :
+            new this.model(doc);
+    }
+
+    // Otherwise, issue the query and store the result in redis
+    const result = await exec.apply(this, arguments);
+
+    client.set(key, JSON.stringify(result));
+
+    return result;
 }
