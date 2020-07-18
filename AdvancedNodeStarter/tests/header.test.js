30ee4e0c2a1f402e28ada275705b78a715577149
thiagoMoraes21 <thiago4x70@gmail.com>
2020-07-18 15:48:47 -0300

    extracting page content with jest

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,16 +1,34 @@
-// We use puppeteer to create browsers
-// and use browsers to create pages.
+/*
+    **********************
+        TEST WORKFLOW
+    **********************
+
+    repeat: {
+        1: 'Launch Chromium',
+        2: 'Navigate to app',
+        3: 'Click on stuff on screen',
+        4: 'Use a DOM selector to retrieve the content of an element',
+        5: 'Write assertion to make sure content is correct'
+    }
+*/
+
+
+/* 
+    We use puppeteer to create browsers
+    and use browsers to create pages. 
+*/
 const puppeteer = require('puppeteer');
-
-test('Adds two number', () => {
-    const sum = 1 + 2;
-
-    expect(sum).toEqual(3);
-});
 
 test('We can launch a browser', async () => {
     const browser = await puppeteer.launch({ 
         headless: false
     });
     const page = await browser.newPage();
+    await page.goto('localhost:3000');
+
+    const text = await page.$eval('a.brand-logo', el => {
+        return el.innerHTML;
+    });
+
+    expect(text).toEqual('Blogster');
 });
