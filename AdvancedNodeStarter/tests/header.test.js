dfe803111a432b11400675d8c5331da3786d7ca1
thiagoMoraes21 <thiago4x70@gmail.com>
2020-07-18 23:32:50 -0300

    genarating sessions and signatures to make authenticated requests inside the tests scripts

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -12,7 +12,6 @@
     }
 */
 
-
 /* 
     We use puppeteer to create browsers
     and use browsers to create pages. 
@@ -29,7 +28,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-    await browser.close();
+    // await browser.close();
 });
 
 test('The header has the correct text', async () => {
@@ -44,4 +43,36 @@ test('Clicking login starts oauth flow', async() => {
     await page.click('.right a');
     const url = await page.url();
     expect(url).toMatch(/accounts\.google\.com/);
+});
+
+
+/*
+    Faking a cookie session to make authenticated requests
+    1. Create a page instance
+    2. Take an existing user ID and genarete a fake session object with it
+    3. Sign the session object with keygrip
+    4. Set the session and signature on our page instance as cookies
+*/
+test.only('When sign in, shows logout button', async() => {
+    const id = "5f08997c84feda0517a14483";
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    }
+
+    const sessionString = Buffer.from(
+        JSON.stringify(sessionObject)
+    ).toString('base64');
+
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+    
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000'); // refresh page
 });
